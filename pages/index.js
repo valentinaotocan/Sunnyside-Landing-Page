f99fe5f3a4307ba1704f6864d2cd07bfdfa46f1b
Valentina Otočan <valentinaotocan@gmail.com>
2022-08-26 21:17:57 +0200

    Add head element

--- pages/index.js
+++ pages/index.js
@@ -9,6 +9,11 @@ import Testimonials from "../components/testimonials/Testimonials";
 export default function Home() {
   return (
     <div className={styles.homepage}>
+      <Head>
+        <title>Sunnyside agency</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon-32x32.png" />
+      </Head>
       <div className={styles.header}>
         <Navbar />
         <h1 className={styles.title}>We are creatives</h1>
