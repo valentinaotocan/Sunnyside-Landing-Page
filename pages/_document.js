8f86fd7c0629515c3d81d97a29f95756a4bec8a3
Valentina Otočan <valentinaotocan@gmail.com>
2022-09-03 17:00:34 +0200

    Add custom document

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon-32x32.png" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,6 @@ export default function Home() {
     <div className={styles.homepage}>
       <Head>
         <title>Sunnyside agency</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="shortcut icon" href="/favicon-32x32.png" />
       </Head>
       <div className={styles.header}>
         <Navbar />
